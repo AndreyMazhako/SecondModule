aa4d7e5c1c4233735ee581dca44da5a503a02526
AndreyMazhako <amazhako@bk.ru>
2024-09-24 14:54:01 +0300

    Мое имя, возраст, питомец и размер ноги.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -4,8 +4,13 @@ class MainClass
 {
     public static void Main()
     {
-        string MyName = "Yra";
-        Console.WriteLine(MyName);
-        Console.ReadKey();
+        string MyName = "Andrey";
+        int age = 25;
+        bool pet = false;
+        double size = 42.5;
+        Console.WriteLine("My name is {0}\n" +
+            "My age is {1}\n" +
+            "Do I have a pet? {2}\n" +
+            "My shoe size is {3}", MyName, age, pet, size);
     }
 }
