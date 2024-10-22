8cc159befd02bef114a8c096b5bfb2b1a79961d5
AndreyMazhako <amazhako@bk.ru>
2024-10-22 12:47:22 +0300

    Итоговый проект Модуля 5.

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,36 +1,105 @@
 ﻿using System;
 
-class MainClass
+class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Напишите что-то");
-        var str = Console.ReadLine();
+        (string name, string surname, int age, bool hasPet, int petCount, string[] petNames, int favoriteColorCount, string[] favoriteColors) = GetUserData();
 
-        Console.WriteLine("Укажите глубину эха");
-        var deep = int.Parse(Console.ReadLine());
-
-        Echo(str, deep);
-
-        Console.ReadKey();
+        PrintUserData(name, surname, age, hasPet, petCount, petNames, favoriteColorCount, favoriteColors);
     }
 
-    static void Echo(string saidworld, int deep)
+    static (string, string, int, bool, int, string[], int, string[]) GetUserData()
     {
-        Console.WriteLine("..." + saidworld);
+        string name, surname;
+        int age, petCount = 0, favoriteColorCount;
+        bool hasPet;
+        string[] petNames = null;
+        string[] favoriteColors = null;
 
-        var modif = saidworld;
+        Console.WriteLine("Введите имя:");
+        name = Console.ReadLine();
 
-        if (modif.Length > 2)
+        Console.WriteLine("Введите фамилию:");
+        surname = Console.ReadLine();
+
+        age = GetValidInt("Введите возраст:", 1); 
+
+        Console.WriteLine("Есть ли у вас питомец? (да/нет)");
+        hasPet = Console.ReadLine().ToLower() == "да";
+
+        if (hasPet)
         {
-            modif = modif.Remove(0, 2);
+            do
+            {
+                Console.WriteLine("Введите количество питомцев:");
+            } while (!int.TryParse(Console.ReadLine(), out petCount) || petCount <= 0);
+
+            petNames = GetPetNames(petCount);
+        }
+    
+
+        favoriteColorCount = GetValidInt("Введите количество любимых цветов:", 1);
+
+        favoriteColors = GetFavoriteColors(favoriteColorCount);
+
+        return (name, surname, age, hasPet, petCount, petNames, favoriteColorCount, favoriteColors);
+    }
+
+    static int GetValidInt(string message, int minValue) 
+    {
+        int value;
+        do
+        {
+            Console.WriteLine(message);
+        } while (!int.TryParse(Console.ReadLine(), out value) || value <= minValue);
+        return value;
+    }
+
+    static string[] GetPetNames(int count)
+    {
+        string[] names = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"Введите кличку питомца {i + 1}:");
+            names[i] = Console.ReadLine();
+        }
+        return names;
+    }
+
+    static string[] GetFavoriteColors(int count)
+    {
+        string[] colors = new string[count];
+        for (int i = 0; i < count; i++)
+        {
+            Console.WriteLine($"Введите любимый цвет {i + 1}:");
+            colors[i] = Console.ReadLine();
+        }
+        return colors;
+    }
+
+    static void PrintUserData(string name, string surname, int age, bool hasPet, int petCount, string[] petNames, int favoriteColorCount, string[] favoriteColors)
+    {
+        Console.WriteLine($"Имя: {name}");
+        Console.WriteLine($"Фамилия: {surname}");
+        Console.WriteLine($"Возраст: {age}");
+        Console.WriteLine($"Наличие питомца: {hasPet}");
+
+        if (hasPet)
+        {
+            Console.WriteLine($"Количество питомцев: {petCount}");
+            Console.WriteLine("Клички питомцев:");
+            foreach (string petName in petNames)
+            {
+                Console.WriteLine(petName);
+            }
         }
 
-        if (deep > 1)
+        Console.WriteLine($"Количество любимых цветов: {favoriteColorCount}");
+        Console.WriteLine("Любимые цвета:");
+        foreach (string color in favoriteColors)
         {
-            Echo(modif, deep - 1);
+            Console.WriteLine(color);
         }
     }
 }
-
-
